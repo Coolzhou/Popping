e42bfd1475270682094abc13bd2e9979fd21c321
leezhou <xeaje0000@126.com>
2018-04-02 18:04:23 +0800

    decayAnimation

--- Popping/ViewController/DecayViewController/DecayViewController.swift
+++ Popping/ViewController/DecayViewController/DecayViewController.swift
@@ -7,13 +7,58 @@
 //
 
 import UIKit
-
-class DecayViewController: UIViewController {
+import pop
+class DecayViewController: UIViewController, POPAnimationDelegate{
+    
+    var dragView: UIControl?
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addDragView()
+    }
+    
+    func addDragView() {
+        
+        dragView = UIControl.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100))
+        dragView?.layer.cornerRadius = 50
+        dragView?.layer.masksToBounds = true
+        dragView?.center = view.center
+        dragView?.backgroundColor = UIColor.init(red: 52/255.0, green: 152/255.0, blue: 219/255.0, alpha: 1)
+        dragView?.addTarget(self, action: #selector(touchDown), for: UIControlEvents.touchUpInside)
+        
+        view.addSubview(dragView!)
+        
+        let pan = UIPanGestureRecognizer.init(target: self, action: #selector(handlePan))
+        dragView?.addGestureRecognizer(pan)
+        
+    }
+    
+    @objc func touchDown(sender:UIControl) {
+        sender.layer.pop_removeAllAnimations()
+    }
+    
+    @objc func handlePan(pan :UIPanGestureRecognizer){
+        
+        let translation = pan.translation(in: view)
+        pan.view?.center = CGPoint.init(x: (pan.view?.center.x)!+translation.x, y: (pan.view?.center.y)! + translation.y)
+        pan.setTranslation(CGPoint(x: 0, y: 0), in: self.view)
+        
+        if pan.state == .ended {
+            
+            let velocity = pan.velocity(in: self.view)
+            if let anim1 = POPDecayAnimation(propertyNamed: kPOPLayerPosition) {
+                anim1.delegate = self
+                anim1.velocity = NSValue.init(cgPoint: velocity)
+                dragView?.layer.pop_add(anim1, forKey: "layerScaleSmallAnimation")
+            }
+        }
+    }
+    
+    func pop_animationDidApply(_ anim: POPAnimation!) {
+        
     }
 
     override func didReceiveMemoryWarning() {
